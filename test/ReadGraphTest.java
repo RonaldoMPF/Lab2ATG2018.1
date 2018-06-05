e1173323e17e619bb54a610be1d47675ab37de47
adielar <adielar@150.165.42.126>
2018-06-05 14:09:41 -0300

    "remoção de erro no teste read Desconnected Graph"

--- test/ReadGraphTest.java
+++ test/ReadGraphTest.java
@@ -120,7 +120,7 @@ public class ReadGraphTest {
             randomFileGraph.readWeightedGraph(randomFileGraphPath);
             Assert.fail("Should throw an exception while reading this graph");
         } catch (Exception e) {
-        	e.printStackTrace();
+        	//e.printStackTrace();
             assertEquals(e.getMessage(), "Invalid formatting in input file:\n"
                     + "The formatting should be:\n"
                     + "<graph size number>\n"
